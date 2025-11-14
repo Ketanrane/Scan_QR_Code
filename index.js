aaf4ca4056f2c128f8f8ec3fb73c8da25a54840b
Ketan Rane <raneketan1231@gmail.com>
2025-11-14 22:50:00 +0530

    Add QR code generation and URL saving functionality
    This code prompts the user for a URL, generates a QR code image from it, and saves the URL to a text file.

--- index.js
+++ index.js
@@ -3,3 +3,31 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from "inquirer";
+import fs from "fs";
+import qr from "qr-image";
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Enter the URL to generate QR code:",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.url;
+
+    // Generate QR Code
+    const qr_png = qr.image(url, { type: "png" });
+    qr_png.pipe(fs.createWriteStream("qr_img.png"));
+
+    // Save URL in text file
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("QR code generated and URL saved to URL.txt");
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
